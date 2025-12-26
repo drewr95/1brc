6c6cbc6c166bfd1cf2ed63a74d19783059ce542b
Drew Rife <drew.rife95@gmail.com>
2025-12-26 15:39:20 -0500

    feat: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+///
+/// @brief Main entry point of the program.
+/// @param argc Argument count.
+/// @param argv Argument vector.
+/// @return Exit status code.
+///
+auto main(const int argc, const char **argv) noexcept -> int {
+  std::cout << "Hello, World!\n";
+  return 0;
+}
